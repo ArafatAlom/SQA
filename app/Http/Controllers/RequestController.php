545fe8540a34c85cae7c33c7ca88598f1c9d3a19
Mizan47 <139835235+Mizan47@users.noreply.github.com>
2024-02-21 08:40:56 +0600

    Update Request Controller with comments

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -7,20 +7,26 @@ use App\Models\RequestMake;
 
 class RequestController extends Controller
 {
-   function addData(Request $req ){
-       $data = new RequestMake ;
+    // Function to add data from the request
+    function addData(Request $req){
+        // Create a new instance of the RequestMake model
+        $data = new RequestMake;
 
-       $data->gmail = $req->gmail ;
-       $data->private_or_batch = $req->private_or_batch ;
-       $data->subject = $req->subject ;
-       $data->no_of_student = $req->no_of_student ;
-       $data->address = $req->address ;
-       $data->phone_no = $req->phone_no ;
-       $data->teacher_id = $req->teacher_id ;
-       $data->opinion = $req->opinion ;
+        // Assign values from the request to the model properties
+        $data->gmail = $req->gmail;
+        $data->private_or_batch = $req->private_or_batch;
+        $data->subject = $req->subject;
+        $data->no_of_student = $req->no_of_student;
+        $data->address = $req->address;
+        $data->phone_no = $req->phone_no;
+        $data->teacher_id = $req->teacher_id;
+        $data->opinion = $req->opinion;
 
-       $data->save( ) ;
+        // Save the data to the database
+        $data->save();
 
-       return view("index");
-   }
+        // Return view for index page after data is saved
+        return view("index");
+    }
 }
+
