77a97b2c1832c05ff876b2e346342e7d843b0038
sayeda358 <139205176+sayeda358@users.noreply.github.com>
2024-02-25 17:53:40 +0600

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,8 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Payment Model
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $address
+ * @property int $teacher_id
+ * @property string $gateway
+ * @property string $pay_no
+ * @property string $trnx
+ * @property float $amount
+ *
+ * @package App\Models
+ */
 class Payment extends Model
 {
     use HasFactory;
-    public $timestamps=false;
+    public $timestamps = false;
 }
