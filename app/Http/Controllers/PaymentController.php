d19b4d5c7faa5aa3de009a6b5bc8874de80c721a
sayeda358 <139205176+sayeda358@users.noreply.github.com>
2024-02-25 18:02:56 +0600

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,28 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Payment;
 
+/**
+ * PaymentController Class
+ *
+ * @package App\Http\Controllers
+ */
 class PaymentController extends Controller
 {
-    //function for inserting payment information
+    /**
+     * Store payment information in the database.
+     *
+     * @param  Request  $req
+     * @return \Illuminate\View\View
+     */
     function payData(Request $req)
     {
-        //variable for payment table in databse
-        $payment= new Payment;
-        $payment->name=$req->name;
-        $payment->email=$req->email;
-        $payment->address=$req->address;
-        $payment->teacher_id=$req->teacher_id;
-        $payment->gateway=$req->gateway;
-        $payment->pay_no=$req->pay_no;
-        $payment->trnx=$req->trnx;
-        $payment->amount=$req->amount;
+        // Instantiate Payment model
+        $payment = new Payment;
+
+        // Assign values from request to the Payment model properties
+        $payment->name = $req->name;
+        $payment->email = $req->email;
+        $payment->address = $req->address;
+        $payment->teacher_id = $req->teacher_id;
+        $payment->gateway = $req->gateway;
+        $payment->pay_no = $req->pay_no;
+        $payment->trnx = $req->trnx;
+        $payment->amount = $req->amount;
+
+        // Save the payment information
         $payment->save();
 
-        
-        echo '<script>alert("Payment Succesfull!!!!!!!")</script>'; //inline javascript for alert
-        
+        // Display success alert using inline JavaScript
+        echo '<script>alert("Payment Successful!")</script>';
+
+        // Redirect to the index view
         return view('index');
-
-
     }
 }
