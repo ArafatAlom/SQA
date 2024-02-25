9dca05ea5e792a1a8665103d6c701b60f68f6a10
sayeda358 <139205176+sayeda358@users.noreply.github.com>
2024-02-25 17:57:13 +0600

    Update Profile.php

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,8 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Profile Model
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $user_id
+ * @property string $email
+ * @property string $gender
+ * @property string $contact_no
+ * @property string $address
+ *
+ * @package App\Models
+ */
 class Profile extends Model
 {
     use HasFactory;
-    public $timestamps=false ;
+    public $timestamps = false;
 }
