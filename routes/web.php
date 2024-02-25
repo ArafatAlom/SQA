8775bc2c160281bab6a25bc7a14cbc7064121409
Ruhul19981 <152523674+Ruhul19981@users.noreply.github.com>
2024-02-26 00:38:39 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RequestController;
@@ -44,4 +45,23 @@ Route::get('request', function () {
 });
 Route::post('request', [ RequestController::class, 'addData' ] );
 
+// Route to display all teachers
 Route::get('/teachers', [TeacherController::class, 'index']);
+
+// Route to create a new teacher (display form)
+Route::get('/teachers/create', [TeacherController::class, 'create']);
+
+// Route to store a newly created teacher
+Route::post('/teachers', [TeacherController::class, 'store']);
+
+// Route to show a specific teacher
+Route::get('/teachers/{teacher}', [TeacherController::class, 'show']);
+
+// Route to update a specific teacher (display form)
+Route::get('/teachers/{teacher}/edit', [TeacherController::class, 'edit']);
+
+// Route to update a specific teacher
+Route::put('/teachers/{teacher}', [TeacherController::class, 'update']);
+
+// Route to delete a specific teacher
+Route::delete('/teachers/{teacher}', [TeacherController::class, 'destroy']);
